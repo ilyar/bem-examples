ec02da0c2b26b78edcffc849f51cf4d5dffbdc2e
Ilya Rogov <ilyar.software@gmail.com>
2014-05-14 01:15:19 +0400

    add someBlock

---
+++ desktop.blocks/someBlock/someBlock.deps.js
@@ -0,0 +1,20 @@
+[
+    {
+        mustDeps: [
+            {
+                elem: 'title',
+                elem: 'body',
+                elem: 'question'
+            },
+            {
+                block: 'input'
+            },
+            {
+                block: 'button',
+                mods: { theme: 'normal', size: 's'}
+            }
+        ],
+        shouldDeps: [
+        ]
+    }
+]
